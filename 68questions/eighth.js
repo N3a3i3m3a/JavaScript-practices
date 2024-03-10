d7822acae5518a512461258af4c8c394b66c1671
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-03-10 20:57:05 +0200

    JavaScript-practices commit

--- 68questions/eighth.js
+++ 68questions/eighth.js
@@ -1,7 +1,10 @@
 //a function that receives an array of and returns the average of numbers
 function calculateAverage(arr) {
-    const sum = arr.reduce((acc, val) => acc + val, 0);
-  return sum / arr.length;
+    let total=0
+    for(let num of arr){
+      total+=num
+    }
+  return total / arr.length;
 }
   let arr=[1,2,4];
   console.log(calculateAverage(arr))
